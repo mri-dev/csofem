015fcecd5109c372a09d9442347caf4b1c92b64c
mri-dev <pocokxtrame@gmail.com>
2018-04-13 17:44:13 +0200

    Save

---
+++ shop/application/views/v1.0/home/index.php
@@ -0,0 +1,12 @@
+<div class="home">
+	<div class="pw">
+		<div class="grid-layout">
+			<div class="grid-row filter-sidebar">
+				<? $this->render('templates/sidebar'); ?>
+			</div>
+			<div class="grid-row inside-content">
+				Tartalom
+			</div>
+		</div>
+	</div>
+</div>
